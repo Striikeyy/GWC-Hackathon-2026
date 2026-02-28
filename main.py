2ffeff8c3df893c7f10280f01b1a0f608963040a
milk-gg <adamc197728@gmail.com>
2026-02-27 17:14:29 -0800

    m

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 from person import Person
 from group import Group
+from inputData import inputPersonData, inputGroupData
 
+# name, subject, zipCode, dateStart, dateEnd
 persons = []
 groups = []
 
-def setup_data():
-    persons.append(Person("Jacob", "algebra2", 90638, "2026-3-01", "2027-12-31"))
-    persons.append(Person("Tony", "geometry", 90638, "2026-4-01", "2027-12-31"))
+MATHCLASSES = ["algebra1", "algebra2", "geometry", "trigonometry", "precalculus", "apcalculusab", "apcalculusbc", "apstatistics"]
 
-    groups.append(Group("tonygeometry", "geometry", 5, "2026-4-15", 90638))
-    groups.append(Group("jacobalgebra2", "algebra2", 5, "2026-3-15", 90638))
+def main():
+    personCount = int(input("How many people registered in the program? "))
+    for i in range(personCount):
+        persons.append(Person(*inputPersonData(MATHCLASSES)))
+    
+    groupCount = int(input("How many groups are registered in the program? "))
+    for i in range(groupCount):
+        groups.append(Group(*inputGroupData(MATHCLASSES)))
+        for j in range(groups[i].maxPersons):   
+            personName = input("Enter the name of the person you want to add to the group (type BLANK for blank): ")
+            if personName != "BLANK":
+                groups[i].addPerson(personName)
+    
+    if (input("Would you like to find a group? (Y/n): ").lower() == 'y'):
+        persons.append(Person(*inputPersonData(MATHCLASSES)))
+        matches = persons[-1].findMatches(groups)
+        if matches is not None:
+            print("Here are the groups that match your criteria :) :")
+            for match in matches:
+                print(match.name)
+        else:
+            print("Sorry, there are no groups that match with you :(.")
+    
+    if (input("Would you like to make a group? (Y/n): ").lower() == 'y'):
+            groups.append(Group(*inputGroupData(MATHCLASSES)))
+            for j in range(groups[-1].maxPersons):   
+                personName = input("Enter the name of the person you want to add to the group (type BLANK for blank): ")
+                if personName != "BLANK":
+                    groups[-1].addPerson(personName)
+
+    print("los tralaleritos")
+
+if __name__ == "__main__": 
+    main()
